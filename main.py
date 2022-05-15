d532bca42aa741add9650014ef95b5d47daf893c
Thanh <nthanhster@gmail.com>
2022-05-15 22:28:58 +0200

    waitkey

--- main.py
+++ main.py
@@ -21,17 +21,21 @@ with open(classFile,'rt') as f:
     net.setInputMean((127.5, 127.5, 127.5))
     net.setInputSwapRB(True)
 
-    while True:
-        success, img = cap.read()
-        classIds, confs, bbox = net.detect(img, confThreshold=thres)
-        print(classIds, bbox)
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    print(classIds, bbox)
 
-        if len(classIds) != 0:
-            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, classNames[classId-1].upper(), (box[0]+10,box[1]+30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.putText(img, str(round(confidence * 100, 2)), (box[0]+200,box[1]+30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.imshow("Output", img)
-                cv2.waitKey(1)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId-1].upper(), (box[0]+10,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0]+200,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow("Output", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
